d5bd61e7785fb338d8582a0810280033ff61b7d2
Ashwin K Ashok <Ashwin Ashok>
2021-02-21 08:16:13 -0500

    fixed the domain url

--- src/common.js
+++ src/common.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://hackbeanpot-backend.herokuapp.com"
-export const DOMAIN = " https://campfire-hbp.herokuapp.com/"
+export const DOMAIN = " https://campfire-hbp.herokuapp.com"
